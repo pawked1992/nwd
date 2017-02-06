64878292d9dc5682b748f315474eb0690df57467
Paweł Kędzierski <pawked1992@gmail.com>
2017-02-06 21:32:46 +0100

    Dodaj pliki projektów.

---
+++ NWD/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj dwie liczby oddzielone spacjami.");
+            string liczby = Console.ReadLine();
+            List<string> lista_danych_wejsciowych = liczby.Split(' ').ToList<string>();
+            sprawdzenie_danych(lista_danych_wejsciowych);
+
+            Console.ReadLine();
+
+        }
+
+        static void sprawdzenie_danych(List<string> dane_wejsciowe)
+        {
+            foreach (var item in dane_wejsciowe)
+            {
+                int var = 0;
+                if (!Int32.TryParse(item,out var))
+                {
+                    Console.WriteLine("Dane wejściowe nieprawidłowe! {0} nie jest liczbą!");
+                    Console.ReadKey();
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                }
+            }
+        }
+
+        static void konwersja_string_na_int(List<string> lista_danych_wejsciowych)
+        {
+            int liczba_pierwsza = Int16.Parse(lista_danych_wejsciowych.First());
+            int liczba_druga = Int16.Parse(lista_danych_wejsciowych.Last());
+            
+            List<int> dzielniki_liczby_pierwszej = new List<int>();
+            List<int> dzielniki_liczby_drugiej = new List<int>();
+
+
+        }
+
+    }
+}
---
+++ NWD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("NWD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NWD")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//Ustawienie wartości false dla elementu ComVisible sprawia, że typy w tym zestawie nie będą widoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("43a55a31-2c6e-4e0f-84a9-c68d8816e1fa")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki 
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
